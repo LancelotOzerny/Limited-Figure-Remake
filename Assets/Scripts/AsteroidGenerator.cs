b333d6108a41b795a3128c1c922949571017b891
Максим Беляков <belyakov@intervolga.ru>
2023-12-19 06:03:34 +0300

    Реализовал генерацию объектов по области

---
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer from;    
+    [SerializeField] private SpriteRenderer to;
+    [SerializeField] private List<GameObject> generateList = new List<GameObject>();
+
+    [SerializeField] private int epsilon = 10_000;
+    private static System.Random rand = new System.Random();
+
+    [SerializeField] private float minTime = 1.0f;
+    [SerializeField] private float maxTime = 2.0f;
+    [SerializeField] private float time;
+
+    private void Start()
+    {
+        time = minTime + rand.Next(0, (int)(epsilon * (maxTime - minTime))) / epsilon;
+    }
+
+    private void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            time = minTime + rand.Next(0, (int)(epsilon * (maxTime - minTime))) / epsilon;
+            Create();
+        }
+    }
+
+    private void Create()
+    {
+        if (generateList.Count == 0)
+        {
+            return;
+        }
+
+        int generateIndex = rand.Next(generateList.Count);
+        GameObject newObj = Instantiate(generateList[generateIndex], transform);
+
+        Vector2 fromPos = from.transform.position;
+
+        newObj.transform.position = new Vector2(
+            rand.Next(
+                (int)(fromPos.x * epsilon), 
+                (int)(fromPos.x * epsilon + from.bounds.size.x * epsilon)
+            ) / epsilon - from.bounds.size.x / 2,
+            from.gameObject.transform.position.y
+        );
+    }
+}
