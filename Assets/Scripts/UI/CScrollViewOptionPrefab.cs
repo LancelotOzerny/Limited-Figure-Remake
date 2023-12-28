6182996f49ee971b22fa41442206b931f726da60
Максим Беляков <belyakov@intervolga.ru>
2023-12-28 23:55:41 +0300

    Интегрировал новую версию GDT

---
+++ Assets/Scripts/UI/CScrollViewOptionPrefab.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CScrollViewOptionPrefab : CPrefValueFloat
+{
+    public override void Save()
+    {
+        base.Save();
+    }
+
+    public override void Load()
+    {
+        base.Load();
+    }
+}
